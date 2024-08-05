4c8eda1109cfca62ee4f5f2bdc8ccd3a7750e5c1
Agnes-milia <ratipotti@gmail.com>
2024-08-05 13:03:28 +0200

    debug1

--- aut11/app/Http/Controllers/ApiController.php
+++ aut11/app/Http/Controllers/ApiController.php
@@ -1,4 +1,13 @@
 <?php
+require 'vendor/autoload.php'; // Autoload Composer csomagok
+
+use DebugBar\StandardDebugBar;
+
+$debugbar = new StandardDebugBar();
+$debugbarRenderer = $debugbar->getJavascriptRenderer();
+
+// Adatok hozzáadása a DebugBar-hoz
+$debugbar["messages"]->addMessage("Hello World!");
 
 namespace App\Http\Controllers;
 
@@ -22,3 +31,20 @@ public function errorResponse($message = "Error", $status = 400)
 }
 
 }
+
+// A HTML kimenetbe illesztés a megfelelő helyen
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php echo $debugbarRenderer->renderHead(); ?>
+</head>
+<body>
+    <h1>My Page</h1>
+    <?php echo $debugbarRenderer->render(); ?>
+</body>
+</html>
