5083fd8c02029682f915fddefc9cf196e0490b57
Agnes-milia <ratipotti@gmail.com>
2024-08-07 15:28:52 +0200

    minden útvonal működik

--- aut11/routes/api.php
+++ aut11/routes/api.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //autentikált végpontok
-Route::middleware(['auth:sanctum')->group(function () {
-    Route::get('/admin', function (Request $request) {
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
         return $request->user();
     });
     // Kijelentkezés útvonal
