627d62163a3cc53e0fe0af8fb48e357c56fb7c56
Agnes-milia <ratipotti@gmail.com>
2024-08-07 15:16:21 +0200

    útvonalak szerkesztve

--- aut11/routes/api.php
+++ aut11/routes/api.php
@@ -5,17 +5,23 @@ use App\Http\Middleware\AdminMW;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//autentikált végpontok
+Route::middleware(['auth:sanctum')->group(function () {
+    Route::get('/admin', function (Request $request) {
+        return $request->user();
+    });
+    // Kijelentkezés útvonal
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
-
+//admin végpontok
 Route::middleware(['auth:sanctum', AdminMW::class])->group(function () {
     Route::get('/admin', function (Request $request) {
         return $request->user();
     });
 });
 
+//bárki által hozzáférhető útvonal
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-// Kijelentkezés útvonal
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
